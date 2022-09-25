79eb3297b7be20c2809164994a38ae7522a96d24
elisavetTriant <elitriant@gmail.com>
2022-09-26 01:13:21 +0300

    Marker Cluster Feature

--- wp-content/themes/twentytwenty-child/src/index.js
+++ wp-content/themes/twentytwenty-child/src/index.js
@@ -1,7 +1,7 @@
 import "../css/style.scss"
 
 // Our modules / classes
-import LeafletMap from "./modules/LeafletMap"
+import LeafletMap from "./modules/LeafletMapClusters"
 
 // Instantiate a new object using our modules/classes
 var leafletMap  = new LeafletMap()
